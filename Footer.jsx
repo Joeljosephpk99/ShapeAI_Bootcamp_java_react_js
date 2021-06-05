e9e73249f0d533fb8e0362afd914c8f2c3f11487
Joeljosephpk99 <84792311+Joeljosephpk99@users.noreply.github.com>
2021-06-06 00:33:58 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+
+  var currYear = new Date().getUTCFullYear();
+
+return (
+  <footer>
+    <p>Copyright @ {currYear}</p>
+    </footer>
+);
+}
+
+export default Footer
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+return (
+  <div className="note">
+    <h1>javascript and react.js</h1>
+    <p> this is amazing bootcamp</p>
+    </div>
+
+);
+}
+
+export default Note
